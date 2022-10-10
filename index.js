5b7d2f8822494ef6e3fb58d0ef9e100715449001
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 03:37:12 +0500

    Редктирование текста

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/web-data', async (req, res) => {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            input_message_content: { message_text: `Поздравляем с покупкой, вы приобрели товар на сумму ${totalPrice}` }
+            input_message_content: { message_text: 'Поздравляем с покупкой, вы приобрели товар на сумму: ' + totalPrice }
         });
         return res.status(200).json({});
     } catch (e) {
