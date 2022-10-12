b88ed47b599820a9b23d99a734cd1636f306b877
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-12 18:45:38 +0500

    no cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new TelegramBot(token, { polling: true });
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Listen for any kind of message. There are different kinds of
 // messages.
