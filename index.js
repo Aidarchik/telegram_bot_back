47c88f62901ee15785306d9a1e332f282c496bfa
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-14 15:47:32 +0500

    WebAppUrl

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 // const webAppUrl = 'https://633eca5bc6b16200b216310f--delightful-boba-ba0a21.netlify.app';
-const webAppUrl = 'https://acca-46-146-21-53.eu.ngrok.io';
+const webAppUrl = 'https://delightful-boba-ba0a21.netlify.app';
 const token = '5677851691:AAGANdpmDqhPv3yIRjeBvNMfIFMjUMWG5kQ';
 
 const bot = new TelegramBot(token, { polling: true });
