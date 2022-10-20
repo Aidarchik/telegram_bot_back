aa90b11733262caa9191f05bb8f3d888e9292a7c
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-20 19:45:48 +0500

    калькулятор

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { token, vkOptions, vkOptionsElectron, tokenElecton } from './auth_data.js';
 import VK from 'node-vk-bot-api';
+import VkBotMarkup from 'node-vk-bot-api/lib/markup.js';
 import https from 'https';
 import fs from 'fs';
 import express from 'express';
@@ -15,10 +16,33 @@ app.use(helmet());
 
 
 const vk = new VK(vkOptionsElectron);
+const markup = new VkBotMarkup();
+
+vk.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (e) {
+        console.error(e);
+    }
+});
 
 vk.on((ctx) => {
     const message = ctx.message?.text;
-    ctx.reply(message);
+    ctx.reply(message, null, markup
+        .keyboard([
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '0',
+        ], { columns: 3 })
+        .oneTime(),
+    );
     console.log(ctx);
 });
 
