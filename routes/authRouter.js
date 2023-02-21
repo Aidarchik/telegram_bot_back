3086133f69bbfe275cd0a43b078db671e54829a4
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2023-02-22 00:18:45 +0500

    sd

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -49,17 +49,3 @@ router.get('/vkontakte/callback',
 
 export default router;
 
-TypeError: Преобразование круговой структуры в JSON
---> начиная с объекта с конструктором 'Socket'
-    | свойство 'parser' -> объект с конструктором 'HTPParser'
---- свойство 'socket' замыкает круг
-    в JSON.stringify(<анонимно>)
-    в stringify (/root/telegram_bot_back/node_modules/express/lib/response.js:1150:12)
-    в ServerResponse.json (/root/telegram_bot_back/node_modules/express/lib/response.js:271:14)
-    в файле:///root/telegram_bot_back/routes/authRouter.js:23:21
-    в Layer.handle [как handle_request] (/root/telegram_bot_back/node_modules/express/lib/router/layer.js:95:5)
-    далее (/root/telegram_bot_back/node_modules/express/lib/router/route.js:144:13)
-    на Route.dispatch (/root/telegram_bot_back/node_modules/express/lib/router/route.js:114:3)
-    в Layer.handle [как handle_request] (/root/telegram_bot_back/node_modules/express/lib/router/layer.js:95:5)
-    в /root/telegram_bot_back/node_modules/express/lib/router/index.js:284:15
-    в Function.process_params (/root/telegram_bot_back/node_modules/express/lib/router/index.js:346:12)
