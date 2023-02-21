50ecf62927689623b04b73302457ff54826975c4
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2023-02-22 00:19:34 +0500

    sd

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -20,7 +20,7 @@ router.get('/login/success', (req, res) => {
         })
     }
     // res.redirect('https://sushilike159.ru/api/auth/login/failed')
-    res.status(500).json(req.body)
+    res.status(500).json(req.headers)
 })
 
 router.get('/login/failed', (req, res) => {
